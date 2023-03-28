7be98b7e9f8c5604f62ecaf143fc10e73c02eada
fianc <dhxofla123@naver.com>
2023-03-28 14:24:00 +0900

    최단경로

---
+++ 이것이코딩테스트다/최단경로/간단한 다익스트라.py
@@ -0,0 +1,56 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미
+
+# 노드의 갯수, 간선의 수
+n, m = map(int, input().split())
+# 시작 노드
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트
+graph = [[] for i in range(n+1)]
+# 방문 여부를 체크하는 리스트
+visited = [False] * (n+1)
+# 최단거리 테이블을 무한으로 초기화
+distance = [INF] * (n+1)
+
+# 간선정보 입력
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+# 방문하지 않은 노드 중에서 가장 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1, n+1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    
+    # 시작 노드를 제외한 전체 n-1개의 노드에 대해 반복
+    for i in range(n-1):
+        now = get_smallest_node()
+        visited[now] = True
+
+        # 현재 노드와 연결된 다른 노드 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    # 도달할 수 없는 경우 INF
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
