46bea1e208165bc2fbca504619c2623adee4c82d
troh <dhxofla91@gmail.com>
2023-08-15 22:05:52 +0900

    백준11725 - dfs

---
+++ DFS&BFS/[백준]11725_트리의 부모찾기.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+graph = [[] for i in range(n)]
+for _ in range(n-1):
+    v1, v2 = map(int, input().split())
+    graph[v1-1].append(v2-1)
+    graph[v2-1].append(v1-1)
+print(graph)
+
+parents = [-1] * n
+visited = [False] * n
+def dfs(v):
+    if visited[v]:
+        return
+    
+    visited[v] = True
+    for link in graph[v]:
+        if not visited[link]:
+            parents[link] = v
+            dfs(link)
+dfs(0)
+for i in range(1, len(parents)):
+    print(parents[i]+1)
