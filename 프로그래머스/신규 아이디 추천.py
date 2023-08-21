c68f1d532c1260f1c4c6b00a56b73f61f611f6e6
dhxofla91 <dhxofla123@naver.com>
2023-08-21 16:12:21 +0900

    push

--- 프로그래머스/신규 아이디 추천.py
+++ 프로그래머스/신규 아이디 추천.py
@@ -1,28 +1,14 @@
 import re
 def solution(new_id):
-    # 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
-    # 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
-    # 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
-    # 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
-    # 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
-    # 6단 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
-    # 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-    # 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
     print(new_id)
     new_id = new_id.lower()                                 # 1
-    print(new_id, "1단계 후")
-    new_id = re.sub('[^a-z0-9\-_.]', '', new_id)        # 2
-    print(new_id, "2단계 후")
-    new_id = re.sub('\.+', '.', new_id)
-    print(new_id, "3단계 후")
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id)            # 2
+    new_id = re.sub('\.+', '.', new_id)                     # 3
+    new_id = re.sub('^[.]|[.]$', '', new_id)                # 4
+    new_id = "a" if len(new_id) == 0 else new_id[:15]       # 56
     new_id = re.sub('^[.]|[.]$', '', new_id)
-    print(new_id, "4단계 후")
-    new_id = "a" if len(new_id) == 0 else new_id[:15]
-    new_id = re.sub('^[.]|[.]$', '', new_id)
-    print(new_id, "56단계 후")
-    while len(new_id) < 3:
-        new_id = new_id + new_id[len(new_id)-1]
-    print(new_id, "7단계 후")
+    while len(new_id) < 3:                                  
+        new_id = new_id + new_id[len(new_id)-1]             #7
 
     answer = new_id
     return answer
--- 프로그래머스/타겟넘버.py
+++ 프로그래머스/타겟넘버.py
@@ -1,27 +1,40 @@
-def dfs(graph, n, cnt):
-    result = []
-    if n == len(graph):
-        return [cnt]
-    else:
-        link = graph[n]
-        for v in link:
-            sum = dfs(graph, n+1, cnt+v)
-            result += sum
-    return result
-     
+from collections import deque
+def solution(maps):
+    n, m = len(maps), len(maps[0])
+    
+    dx = [1, 0, -1, 0]
+    dy = [0, -1, 0, 1]
+    
+    q = deque()
+    q.append([0, 0])
+    
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+            
+            # 처음 방문한 노드, 이전 노드까지 이동한 거리 +1
+            if maps[ny][nx] == 1:
+                q.append([nx, ny])
+                maps[ny][nx] = maps[y][x]+1
+            # 이미 방문한 적이 있는 노드인 경우, 더 빠른 경로인 경우만 큐에 추가
+            elif maps[ny][nx] != 0 and maps[ny][nx] != 1:
+                if maps[y][x]+1 < maps[ny][nx]:
+                    q.append([nx, ny])
+                    maps[ny][nx] = min(maps[ny][nx], maps[y][x]+1)
 
-def solution(numbers, target):
-    graph = []
-    for i in range(len(numbers)):
-        graph.append([numbers[i], numbers[i]*-1])
+    # 목적지까지 이동한 횟수
+    answer = maps[n-1][m-1]
 
-    answer = 0
-    result = dfs(graph, 0, 0)
-    for r in result:
-        if target == r:
-            answer += 1
+    # 목적지까지 이동 가능한 경우 이동횟수가 최소 2이상일 수 있음,
+    if answer == 1:
+        answer = -1
     return answer
 
-print(solution([1, 2]	,3))
-print(solution([1, 1, 1, 1, 1]	,3))
-print(solution([4, 1, 2, 1]		,4))
+print(solution([[1,0,1,1,1],
+                [1,0,1,0,1],
+                [1,0,1,1,1],
+                [1,1,1,0,1],
+                [0,0,0,0,1]]	))
