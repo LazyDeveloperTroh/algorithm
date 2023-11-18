6281cc81f58886e47b0a5036d189092cd3533342
troh <dhxofla123@naver.com>
2023-11-18 18:03:57 +0900

    백준 1380 - 플로이드로 풀어보기

--- DFS&BFS/[백준]1380_케빈 베이컨의 6단계 법칙.py
+++ DFS&BFS/[백준]1380_케빈 베이컨의 6단계 법칙.py
@@ -3,42 +3,78 @@
 #   DFS로 푸는 경우 (1,2) (1,3) (2,3) 케이스의 경우 1부터 3까지의 거리가 2로 나올 수 있음
 # 그래서 BFS로 문제를 풀어서 정답 판정을 받았다.
 # 노드의 개수도 100개밖에 없기 때문에 플로이드워셜 알고리즘을 통해 최적화가 가능한 것을 알았다.
-from collections import deque
-import sys
-input = sys.stdin.readline
 
-# n 인원수, m 관계의 수
+# BFS 풀이
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+
+# # n 인원수, m 관계의 수
+# n, m = map(int, input().split())
+
+# # 그래프 초기화
+# graph = [[] for _ in range(n+1)]
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+
+# # bfs로 target까지 최단거리 구함
+# def bfs(start, target, visited):
+#     q = deque()
+#     q.append((start, 0))
+#     visited[start] = True
+
+#     while q:
+#         p, cnt = q.popleft()
+#         if p == target:
+#             return cnt
+        
+#         for i in graph[p]:
+#             if visited[i] == False:
+#                 q.append((i, cnt+1))
+#                 visited[i] = True
+
+# # 출력
+# result = [int(1e9)]
+# for i in range(1, n+1):
+#     total = 0
+#     for j in range(1, n+1):
+#         visited = [False]*(n+1)
+#         total += bfs(i, j, visited)
+#     result.append(total)
+# print(result.index(min(result)))
+
+# 플로이드워셜 풀이
 n, m = map(int, input().split())
 
 # 그래프 초기화
-graph = [[] for _ in range(n+1)]
+INF = int(1e9)
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-# bfs로 target까지 최단거리 구함
-def bfs(start, target, visited):
-    q = deque()
-    q.append((start, 0))
-    visited[start] = True
+# 점화식
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
 
-    while q:
-        p, cnt = q.popleft()
-        if p == target:
-            return cnt
-        
-        for i in graph[p]:
-            if visited[i] == False:
-                q.append((i, cnt+1))
-                visited[i] = True
-
-# 출력
-result = [int(1e9)]
+result = [INF]
 for i in range(1, n+1):
     total = 0
     for j in range(1, n+1):
-        visited = [False]*(n+1)
-        total += bfs(i, j, visited)
+        if graph[i][j] == INF:
+            continue
+        total += graph[i][j]
     result.append(total)
+
 print(result.index(min(result)))
